cb4a72e13cecb0bbe309e4050ea46e549c6b582a
Ionut Muresan <64604859+ionutmuresan15@users.noreply.github.com>
2025-05-06 17:39:50 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+document.cookie = "name=Oeschger; SameSite=None; Secure";
+document.cookie = "favorite_food=tripe; SameSite=None; Secure";
+
 function showCookies() {
   const output = document.getElementById("cookies");
   output.textContent = `> ${document.cookie}`;
