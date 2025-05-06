2e5ed76f434bb6f4e68d4a6facca70d7b4d1ac65
Ionut Muresan <64604859+ionutmuresan15@users.noreply.github.com>
2025-05-06 18:10:32 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 document.cookie = "name=Oeschger; SameSite=None; Secure";
 document.cookie = "favorite_food=tripe; SameSite=None; Secure";
+document.cookie = "test1=Hello; SameSite=None; Secure";
+document.cookie = "test2=World; SameSite=None; Secure";
 
 function showCookies() {
   const output = document.getElementById("cookies");
@@ -9,4 +11,19 @@ function showCookies() {
 function clearOutputCookies() {
   const output = document.getElementById("cookies");
   output.textContent = "";
+
+const cookieValue = document.cookie
+  .split("; ")
+  .find((row) => row.startsWith("test2="))
+  ?.split("=")[1];
+
+function showCookieValue() {
+  const output = document.getElementById("cookie-value");
+  output.textContent = `> ${cookieValue}`;
+}
+
+function clearOutputCookieValue() {
+  const output = document.getElementById("cookie-value");
+  output.textContent = "";
+}
 }
